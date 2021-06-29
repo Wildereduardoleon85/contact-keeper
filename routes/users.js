79753ddb6915814c878175fcf735003480935224
Wilder León <wilder_eduardo85@hotmail.com>
2021-06-29 13:31:13 -0400

    create and respond with json web token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -25,15 +27,29 @@ router.post('/',
             if (user){
                 return res.status(400).json({msg: 'User alredy exists'})
             }    
+            
             user = new User({
                 name: name,
                 email: email,
                 password: password
             });
+
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
             await user.save();
-            res.send('User Saved!!')
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'), {
+                expiresIn: 360000
+            }, (err, token)=>{
+                if(err) throw err;
+                res.json({token});
+            });
+
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
