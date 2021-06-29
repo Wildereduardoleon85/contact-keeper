c4355e29ea7542af1899572fee3740018924d7e1
Wilder León <wilder_eduardo85@hotmail.com>
2021-06-29 13:16:15 -0400

    hash password and Register route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 
@@ -10,15 +11,33 @@ const User = require('../models/User');
 router.post('/', 
     body('name', 'Please add a name').not().isEmpty(),
     body('email', 'You must type a valid Email').isEmail(),
-    body('password', 'Password must be at least 4 characters').isLength({ min: 4}),
+    body('password', 'Password must be at least 4 characters').isLength({ min: 6}),
 
-    (req, res)=>{
+    async (req, res)=>{
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
           }
 
-        res.send('user created!!')
+        const {name, email, password} = req.body;
+        try {
+            let user = await User.findOne({email: email});
+            if (user){
+                return res.status(400).json({msg: 'User alredy exists'})
+            }    
+            user = new User({
+                name: name,
+                email: email,
+                password: password
+            });
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            res.send('User Saved!!')
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     })
 
 module.exports = router;
