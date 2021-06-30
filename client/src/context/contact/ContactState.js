e9c9375d83acfc449a7aef450a7e767a630de59a
Wilder León <wilder_eduardo85@hotmail.com>
2021-06-30 16:00:57 -0400

    edit and update contact action

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -63,6 +63,9 @@ const ContactState = props => {
     };
 
     //Update contact
+    const updateContact = contact => {
+        dispatch({type: UPDATE_CONTACT, payload: contact});
+    };
 
     //Filter contacts
 
@@ -76,7 +79,8 @@ const ContactState = props => {
                 addContact,
                 deleteContact,
                 setCurrent,
-                clearCurrent
+                clearCurrent,
+                updateContact
             }}
         >
             {props.children}
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -15,6 +15,11 @@ export default (state, action) => {
                 ...state,
                 contacts: [...state.contacts, action.payload]
             };
+        case UPDATE_CONTACT:
+            return{
+                ...state,
+                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+            }
         case DELETE_CONTACT:
             return{
                 ...state,
@@ -29,7 +34,7 @@ export default (state, action) => {
             return{
                 ...state,
                 current: null
-            }
+            };
         default:
             return  state;
     }
