02e9f6742937f9a09f86abe0f501cc39dc6f0483
Wilder León <wilder_eduardo85@hotmail.com>
2021-07-01 17:01:46 -0400

    user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-export const Login = () => {
+export const Login = (props) => {
+    const alertContext = useContext(AlertContext);
+    const authContext = useContext(AuthContext);
+
+    const {setAlert} = alertContext;
+    const {login, error, clearErrors, isAuthenticated} = authContext;
+
+    useEffect(()=>{
+        if(isAuthenticated){
+            props.history.push('/');
+        }
+
+        if(error === 'Invalid Credentials'){
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+        // eslint-disable-next-line
+    }, [error, isAuthenticated, props.history]);
+
     const [user, setUser] = useState({
         email: '',
         password: ''
@@ -14,8 +34,15 @@ export const Login = () => {
 
     const onSubmit = (e)=>{
         e.preventDefault();
-        console.log('Login submit');
-    }
+        if(email === '' || password === ''){
+            setAlert('Please fill in all fileds', 'danger')
+        }else{
+            login({
+                email, 
+                password
+            });
+        }  
+    };
 
     return (
         <div className="form-container">
@@ -25,11 +52,11 @@ export const Login = () => {
             <form onSubmit={onSubmit}>
                 <div className="form-group">
                     <label htmlFor="email">Email Adress</label>
-                    <input type="email" name="email" value={email} onChange={onChange} />
+                    <input type="email" name="email" value={email} onChange={onChange} required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" value={password} onChange={onChange} />
+                    <input type="password" name="password" value={password} onChange={onChange} required/>
                 </div>
                 <input type="submit" vlaue="Login" className="btn btn-primary btn-block" />
             </form>
